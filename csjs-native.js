7b06d87b902e64f5ffb3ed8541eb9ea4a2e3ab39
hareeqi <hareeqi@gmail.com>
2016-12-04 21:51:21 +0100

    first commit

---
+++ csjs-native.js
@@ -0,0 +1,55 @@
+
+var parse = require('css-parse')
+
+var sheet;
+var create
+
+/*
+  Transform code is taken from https://github.com/raphamorim/native-css
+  Much apperiated ...
+*/
+function transform(rules, result) {
+    rules.forEach(function (rule) {
+        var obj = {};
+        if (rule.type === 'media') {
+            var name = '@media ' +rule.media;
+            var media = result[name] = result[name] || {
+                "__expression__": rule.media
+            };
+            transform( rule.rules, media)
+        } else if (rule.type === 'rule') {
+            rule.declarations.forEach(function (declaration) {
+                if (declaration.type === 'declaration') {
+                   declaration.property = toCamelCase(declaration.property)
+                    obj[declaration.property] = declaration.value;
+                }
+            });
+            rule.selectors.forEach(function (selector) {
+                var name = nameGenerator(selector.trim());
+                result[name] = obj;
+            });
+        }
+    });
+}
+var nameGenerator = function (name) {
+    name = name.replace(/\s\s+/g, ' ');
+    name = name.replace(/[^a-zA-Z0-9]/g, '_');
+    name = name.replace(/^_+/g, '');
+    name = name.replace(/_+$/g, '');
+    return name;
+}
+var toCamelCase = function(name) {
+    return name.replace(/(-.)/g, function(v) { return v[1].toUpperCase(); })
+}
+
+module.exports.csjs = (css) =>{
+  css =  parse(css[0]);
+  var result = {};
+  transform(css.stylesheet.rules, result);
+  result = create? create(result) : result;
+  return result;
+
+}
+module.exports.setStyleSheet = (styleSheet) => {
+  sheet = styleSheet;
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+var csjs = require('./csjs-native.js')
+
+module.exports = csjs.csjs
+module.exports.csjs = csjs.csjs
+module.exports.setStyleSheet = csjs.setStyleSheet
